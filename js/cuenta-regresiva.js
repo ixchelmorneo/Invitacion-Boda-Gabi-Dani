bd3be4c658f443feebdb0e0012deda75404813bc
Ixchel Moreno <ixchel_sidek@hotmail.com>
2025-10-08 03:21:22 -0600

    cambiando tema,contador regresivo,fecha,agregacion de fotos y modificacion de itinerario

--- js/cuenta-regresiva.js
+++ js/cuenta-regresiva.js
@@ -1,6 +1,6 @@
 
 // Definir manualmente la fecha para la cuenta regresiva
-var fechaCuentaRegresiva = 'Apr 26, 2026 17:00:00';  // Cambia esta fecha según lo que necesites
+var fechaCuentaRegresiva = 'Dec 6, 2025 17:00:00';  // Cambia esta fecha según lo que necesites
 var countDownDate = new Date(fechaCuentaRegresiva).getTime();
 
 // Actualizar el contador cada 1 segundo
