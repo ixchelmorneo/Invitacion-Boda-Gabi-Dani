a0e0a5f47c881cef87f764133a5f8bf12f786746
Ixchel Moreno <ixchel_sidek@hotmail.com>
2025-10-08 20:12:24 -0600

    cambiando imagen de portada once intento

--- js/portada.js
+++ js/portada.js
@@ -1,4 +1,4 @@
-/* Función para verificar el soporte de WebP
+// Función para verificar el soporte de WebP
 function support_format_webp() {
   var elem = document.createElement('canvas');
   if (!!(elem.getContext && elem.getContext('2d'))) {
@@ -10,8 +10,16 @@ function support_format_webp() {
 // Cambiar la imagen de fondo según soporte WebP
 window.onload = function () {
   var imageParallax = support_format_webp() ? 'source/img/galeria/inicio-yk.jpg' : 'source/img/galeria/inicio-yk.jpg';
-  document.querySelector('.portada').style.backgroundImage = `url(${imageParallax})`;
-};*/
+  
+  // Aplicar la imagen de fondo con propiedades responsive
+  var portada = document.querySelector('.portada');
+  if (portada) {
+    portada.style.backgroundImage = `url(${imageParallax})`;
+    portada.style.backgroundSize = 'cover';        // Hace la imagen responsive
+    portada.style.backgroundPosition = 'center';   // Centra la imagen
+    portada.style.backgroundRepeat = 'no-repeat';  // Evita que se repita
+  }
+};
 
 
 
